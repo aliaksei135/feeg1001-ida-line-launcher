645b08070eb9a6e16eb68cffb121872607ea4d73
Aliaksei Pilko <aliakseipilko1@gmail.com>
2018-01-14 19:13:39 +0000

    Properly impl multithreading

--- main.py
+++ main.py
@@ -1,37 +1,55 @@
-from pathos.multiprocessing import ProcessingPool as Pool
-import dill
+from multiprocessing import Pool
 
-from matplotlib.colors import Normalize
-
+import matplotlib.pyplot as pl
+import numpy as np
 import plotly
-import plotly.plotly as py
 import plotly.graph_objs as go
-import webbrowser
+import plotly.plotly as py
 
 import spring_calc
 import test_data
 import traj_calc
-import scipy.optimize as opt
-import numpy as np
-import matplotlib.pyplot as pl
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
 
-def make_calc(target_height, height_at_x):
-    # Constants
-    k = 500
-    #Vars
-    init_deflection = 0.1
-    alpha = 45
-    exit_v = spring_calc.do_calc(k, init_deflection, alpha)
-    vX, vY, x, h = traj_calc.do_calc(exit_v, alpha)
+
+## GLOBAL CONSTS ##
+# global k, \
+#     startx, deltax, endx, \
+#     startalpha, deltaalpha, endalpha, \
+#     target_dist, target_height_at_dist, \
+#     x_deflections, y_alphas, z_height_at_target
 
 
 def find_nearest(flt_array, target, tol):
     return (np.abs(flt_array - target)).argmin()
 
+def do_iter(x_count, x, ys, target_dist, k):
+    y_count = 0
+    zs = []
+    for y in ys:
+        exit_v = spring_calc.do_calc(k, x, y)
+        # only need h here but the rest of can come along for the ride as well
+        vX, vY, x_dist, h = traj_calc.do_calc(exit_v, y, stop_at=target_dist)
+        try:
+            if max(x_dist) < target_dist:
+                raise StopIteration
+            else:
+                target_dist_index = find_nearest(x_dist, target_dist, 0.04)
+        except StopIteration:
+            print('Target Distance not achieved')
+            zs.append(0)
+            continue
+        zs.append(h[target_dist_index])
+        y_count += 1
+    print('Completed iters for x={}'.format(x_count))
+    return zs
 
-def make_full_plot():
+
+def make_full_plot(multithread=True):
+    # global k, \
+    #     startx, deltax, endx, \
+    #     startalpha, deltaalpha, endalpha, \
+    #     target_dist, target_height_at_dist, \
+    #     x_deflections, y_alphas, z_height_at_target
     #Constants
     k = 1310 #Spring const [Nm]
     startx = 0.04 # Min spring deflection
@@ -48,76 +66,46 @@ def make_full_plot():
     z_height_at_target = np.zeros((len(x_deflections), len(y_alphas)), dtype=np.float64)
 
     # Probably inaccurate to the the point of being arbitrary but still looks cool :)
-    print('Expected time to complete: {} seconds = {} minutes'.format(((len(x_deflections) * len(y_alphas))/3.2), ((len(x_deflections) * len(y_alphas))/3.2)/60))
+    print('[MULTITHREAD] Expected time to complete: {} seconds = {} minutes'.format(((len(x_deflections) * len(y_alphas))/8.4), ((len(x_deflections) * len(y_alphas))/8.4)/60))
 
-    ## SINGLE THREAD ##
-    i = 0
-    x_count = 0
-    miss_count = 0
-    for x in x_deflections:
-        y_count = 0
-        for y in y_alphas:
-            exit_v = spring_calc.do_calc(k, x, y)
-            # only need h here but the rest of can come along for the ride as well
-            vX, vY, x_dist, h = traj_calc.do_calc(exit_v, y, stop_at=target_dist)
-            try:
-                if max(x_dist) < target_dist:
-                    raise StopIteration
-                else:
-                    target_dist_index = find_nearest(x_dist, target_dist, 0.04)
-            except StopIteration:
-                print('Target Distance not achieved')
-                miss_count += 1
-                continue
-            z_height_at_target[x_count, y_count] = h[target_dist_index]
-            print('Completed iter', i+1)
-            i += 1
-            y_count += 1
-        x_count += 1
-    ## /> ##
+    if(not multithread):
+        ## SINGLE THREAD ##
+        i = 0
+        x_count = 0
+        miss_count = 0
+        for x in x_deflections:
+            y_count = 0
+            for y in y_alphas:
+                exit_v = spring_calc.do_calc(k, x, y)
+                # only need h here but the rest of can come along for the ride as well
+                vX, vY, x_dist, h = traj_calc.do_calc(exit_v, y, stop_at=target_dist)
+                try:
+                    if max(x_dist) < target_dist:
+                        raise StopIteration
+                    else:
+                        target_dist_index = find_nearest(x_dist, target_dist, 0.04)
+                except StopIteration:
+                    print('Target Distance not achieved')
+                    miss_count += 1
+                    continue
+                z_height_at_target[x_count, y_count] = h[target_dist_index]
+                print('Completed iter', i+1)
+                i += 1
+                y_count += 1
+            x_count += 1
 
-    ## MULTITHREADED ##
-    ## Commented out non-atomic statements ##
-    ## OOO for now ##
-    # def do_iter(x_count):
-    #     y_count = 0
-    #     x = x_deflections[x_count]
-    #     zs = []
-    #     for y in y_alphas:
-    #         exit_v = spring_calc.do_calc(k, x, y)
-    #         # only need h here but the rest of can come along for the ride as well
-    #         vX, vY, x_dist, h = traj_calc.do_calc(exit_v, y, stop_at=target_dist)
-    #         try:
-    #             if max(x_dist) < target_dist:
-    #                 raise StopIteration
-    #             else:
-    #                 target_dist_index = find_nearest(x_dist, target_dist, 0.04)
-    #         except StopIteration:
-    #             print('Target Distance not achieved')
-    #             # miss_count += 1
-    #             continue
-    #         zs[x_count] = h[target_dist_index]
-    #         # print('Completed iter', i + 1)
-    #         # i += 1
-    #         y_count += 1
-    #     print('Completed iters for x={}'.format(x_count))
-    #     return zs
-    #
-    # pool = Pool(6)
-    # ar = [0 for i in range(len(x_deflections))]
-    # for x_count in range(len(x_deflections)):
-    #     z_height_at_target[x_count:] = pool.apipe(do_iter, (x_count,)).get()
-    # pool.close()
-    # pool.join()
-
-    ## /> ##
-
-    # if miss_count > 50:
-    #     print("Target Distance not achieved {} times. Optimise starting variables?".format(miss_count))
+        if miss_count > 50:
+            print("Target Distance not achieved {} times. Optimise starting variables?".format(miss_count))
+        ## /> ##
+    else:
+        ## MULTITHREADED ##
+        pool = Pool(8)
+        res = [pool.apply_async(do_iter, (count, x_val, y_alphas[0:], target_dist, k)) for count, x_val in enumerate(x_deflections)]
+        for idx, zs in enumerate(res):
+            z_height_at_target[idx] = res[idx].get()
+        ## /> ##
 
 
-    # X, Y = np.meshgrid(x_deflections, np.rad2deg(y_alphas), indexing='xy')
-    # Z = z_height_at_target.T
     X = x_deflections
     Y = np.rad2deg(y_alphas)
     Z = z_height_at_target.T
